9f8ea48476dbeec485ecb6019094e1493441e061
ohsang14 <132821751+ohsang14@users.noreply.github.com>
2023-11-26 17:16:25 +0900

    Add files via upload

--- firstproject/src/main/java/com/example/firstproject/controller/MemberController.java
+++ firstproject/src/main/java/com/example/firstproject/controller/MemberController.java
@@ -7,7 +7,9 @@ import lombok.ToString;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -16,6 +18,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 public class MemberController {
     @Autowired
     MemberRepository memberRepository;
+
+    @GetMapping("/members/new")
+    public String newMemberForm(){
+        return "members/new";
+    }
 
     @GetMapping("/signup")
     public String signUpPage(){
@@ -32,6 +39,18 @@ public class MemberController {
         Member saved = memberRepository.save(member);
         log.info(saved.toString());
         //System.out.println(saved.toString());
-        return "";
+        return "redirect:/members/" + saved.getId();
+    }
+    @GetMapping("/members/{id}")
+    public String show(@PathVariable Long id, Model model){
+        Member member = memberRepository.findById(id).orElse(null);
+        model.addAttribute("member",member);
+        return "members/show";
+    }
+    @GetMapping("/members")
+    public String index(Model model){
+        Iterable<Member>members = memberRepository.findAll();
+        model.addAttribute("members",members);
+        return "members/index";
     }
 }
--- firstproject/src/main/java/com/example/firstproject/entity/Member.java
+++ firstproject/src/main/java/com/example/firstproject/entity/Member.java
@@ -4,8 +4,17 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
+@Getter
 @Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+
 public class Member {
     @Id
     @GeneratedValue
@@ -18,18 +27,4 @@ public class Member {
     @Column
     String password;
 
-    public Member(Long id, String email, String password) {
-        this.id = id;
-        this.email = email;
-        this.password = password;
-    }
-
-    @Override
-    public String toString() {
-        return "Member{" +
-                "id=" + id +
-                ", email='" + email + '\'' +
-                ", password='" + password + '\'' +
-                '}';
-    }
 }
